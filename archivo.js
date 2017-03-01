ecfda64f48654126ee2e7e9abd8deeaf476b0b8e
Erika Vidal <diann852@gmail.com>
2017-03-01 11:17:30 -0500

    haciendo funciones

--- archivo.js
+++ archivo.js
@@ -1,4 +1,6 @@
-var peso = document.getElementById('peso');
-var talla = document.getElementById('talla');
-
-//alert(peso/(talla*talla));
+  function enviaDatos(){
+    var talla = document.getElementById('talla').value;
+    var peso = document.getElementById('peso').value;
+    var imc= parseFloat(peso/Math.pow(talla,2));
+    document.getElementById("datos").innerHTML="</br>" + "IMC : " + imc;
+}
